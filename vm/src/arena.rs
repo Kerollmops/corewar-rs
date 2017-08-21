6a4764872d4b01b1f2c2c38262592e69f27585bd
Kerollmops <renault.cle@gmail.com>
2017-08-21 13:31:27 +0200

    test: Add tests to Arena Read and Write

--- vm/src/arena.rs
+++ vm/src/arena.rs
@@ -51,20 +51,18 @@ pub struct ArenaReader<'a> {
     arena: &'a Arena,
 }
 
+// TODO: use memcpy
 impl<'a> Read for ArenaReader<'a> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let mut buf_index = 0;
-        let memory = self.arena.memory;
-
         while buf_index != buf.len() {
-            buf[buf_index] = memory[self.index];
+            buf[buf_index] = self.arena.memory[self.index];
             buf_index += 1;
             self.index += 1;
-            if self.index == memory.len() {
+            if self.index == self.arena.memory.len() {
                 self.index = 0;
             }
         }
-
         Ok(buf.len())
     }
 }
@@ -74,20 +72,18 @@ pub struct ArenaWriter<'a> {
     arena: &'a mut Arena,
 }
 
+// TODO: use memcpy
 impl<'a> Write for ArenaWriter<'a> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let mut buf_index = 0;
-        let mut memory = self.arena.memory;
-
         while buf_index != buf.len() {
-            memory[self.index] = buf[buf_index];
+            self.arena.memory[self.index] = buf[buf_index];
             buf_index += 1;
             self.index += 1;
-            if self.index == memory.len() {
+            if self.index == self.arena.memory.len() {
                 self.index = 0;
             }
         }
-
         Ok(buf.len())
     }
 
@@ -99,9 +95,45 @@ impl<'a> Write for ArenaWriter<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use core::MEM_SIZE;
 
     #[test]
-    fn it_works() {
-        //
+    fn write_read_at_zero() {
+        let mut arena = Arena::new();
+        let index = ArenaIndex::from_raw(0);
+
+        {
+            let mut writer = arena.write_to(index);
+            let written = writer.write(&[42, 43, 44]).unwrap();
+            assert_eq!(written, 3);
+        }
+
+        {
+            let mut reader = arena.read_from(index);
+            let mut buf = [4, 4, 4];
+            let read = reader.read(&mut buf).unwrap();
+            assert_eq!(read, 3);
+            assert_eq!(&[42, 43, 44], &buf);
+        }
+    }
+
+    #[test]
+    fn write_read_at_limit() {
+        let mut arena = Arena::new();
+        let index = ArenaIndex::from_raw(MEM_SIZE - 2);
+
+        {
+            let mut writer = arena.write_to(index);
+            let written = writer.write(&[42, 43, 44]).unwrap();
+            assert_eq!(written, 3);
+        }
+
+        {
+            let mut reader = arena.read_from(index);
+            let mut buf = [4, 4, 4];
+            let read = reader.read(&mut buf).unwrap();
+            assert_eq!(read, 3);
+            assert_eq!(&[42, 43, 44], &buf);
+        }
     }
 }
